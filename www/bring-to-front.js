f34aceb97b9027e4f544177417e12623dad1c057
brunoPetrim <brunomoreirarocha@gmail.com>
2018-06-07 00:26:22 -0300

    Update bring-to-front.js
    alt

--- www/bring-to-front.js
+++ www/bring-to-front.js
@@ -1,3 +1,17 @@
-module.exports = function() {
-  cordova.exec(null, null, "bringtofront", "bringToFront", [])
+function BringToFront() {
 }
+
+BringToFront.prototype.bringToFront = function (successCallback, errorCallback) {
+  cordova.exec(null, null, "bringtofront", "bringToFront", []);
+};
+
+BringToFront.install = function () {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+
+  window.plugins.bringtofront = new BringToFront();
+  return window.plugins.bringtofront;
+};
+
+cordova.addConstructor(BringToFront.install);
